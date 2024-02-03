c567b42aaad78aaf3692c47efc2f13c913282bcb
Ken Utsunomiya <kenutsunomiya08@gmail.com>
2024-02-03 15:24:34 +0900

    Time: 5 ms (60.45%) | Memory: 44.7 MB (37.46%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -8,46 +8,17 @@ class Solution {
             'D', 500,
             'M', 1000
     );
-    
+
     public int romanToInt(String s) {
         int result = 0;
-        int idx = 0;
-        while (idx < s.length()) {
-            switch (s.charAt(idx)) {
-                case 'I':
-                    if (idx + 1 < s.length() && (s.charAt(idx + 1) == 'V' || s.charAt(idx + 1) == 'X')) {
-                        result += symbolToValue.get(s.charAt(idx + 1)) - symbolToValue.get(s.charAt(idx));
-                        idx += 2;
-                    } else {
-                        result += symbolToValue.get(s.charAt(idx));
-                        idx++;
-                    }
-                    break;
-                case 'X':
-                    if (idx + 1 < s.length() && (s.charAt(idx + 1) == 'L' || s.charAt(idx + 1) == 'C')) {
-                        result += symbolToValue.get(s.charAt(idx + 1)) - symbolToValue.get(s.charAt(idx));
-                        idx += 2;
-                    } else {
-                        result += symbolToValue.get(s.charAt(idx));
-                        idx++;
-                    }
-                    break;
-                case 'C':
-                    if (idx + 1 < s.length() && (s.charAt(idx + 1) == 'D' || s.charAt(idx + 1) == 'M')) {
-                        result += symbolToValue.get(s.charAt(idx + 1)) - symbolToValue.get(s.charAt(idx));
-                        idx += 2;
-                    } else {
-                        result += symbolToValue.get(s.charAt(idx));
-                        idx++;
-                    }
-                    break;
-                default:
-                    result += symbolToValue.get(s.charAt(idx));
-                    idx++;
-                    break;
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && symbolToValue.get(s.charAt(i)) < symbolToValue.get(s.charAt(i + 1))) {
+                result -= symbolToValue.get(s.charAt(i));
+            } else {
+                result += symbolToValue.get(s.charAt(i));
             }
         }
-        
+
         return result;
     }
 }
