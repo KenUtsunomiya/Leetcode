6963c796a580c32a2aef7a72dc0417ec4697395c
Ken Utsunomiya <kenutsunomiya08@gmail.com>
2024-02-04 10:45:50 +0900

    Time: 9 ms (8.99%) | Memory: 42.1 MB (11.20%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,31 +1,14 @@
 class Solution {
+    // sort alphabetically
     public String longestCommonPrefix(String[] strs) {
-    
-        String[] sortedStrs = Arrays.stream(strs)
-                .sorted(Comparator.comparingInt(String::length))
-                .toArray(String[]::new);
-
-        String commonPrefix = sortedStrs[0];
-        for (String str : sortedStrs) {
-            commonPrefix = getCommonPrefix(commonPrefix, str);
-            if (commonPrefix.isEmpty()) {
-                return "";
-            }
-        }
-        
-        return commonPrefix;
-    }
-    
-    private String getCommonPrefix(String s1, String s2) {
-        int i1 = 0;
-        int i2 = 0;
+        String[] sortedStrs = Arrays.stream(strs).sorted().toArray(String[]::new);
+        String first = sortedStrs[0];
+        String last = sortedStrs[sortedStrs.length - 1];
         String commonPrefix = "";
         
-        while (i1 < s1.length() && i2 < s2.length()) {
-            if (s1.charAt(i1) == s2.charAt(i2)) {
-                commonPrefix += s1.charAt(i1);
-                i1++;
-                i2++;
+        for (int i = 0; i < Math.min(first.length(), last.length()); i++) {
+            if (first.charAt(i) == last.charAt(i)) {
+                commonPrefix += first.charAt(i);
             } else {
                 break;
             }
@@ -33,4 +16,40 @@ class Solution {
         
         return commonPrefix;
     }
+
+    // sort by length
+    // public String longestCommonPrefix(String[] strs) {
+    
+    //     String[] sortedStrs = Arrays.stream(strs)
+    //             .sorted(Comparator.comparingInt(String::length))
+    //             .toArray(String[]::new);
+
+    //     String commonPrefix = sortedStrs[0];
+    //     for (String str : sortedStrs) {
+    //         commonPrefix = getCommonPrefix(commonPrefix, str);
+    //         if (commonPrefix.isEmpty()) {
+    //             return "";
+    //         }
+    //     }
+        
+    //     return commonPrefix;
+    // }
+    
+    // private String getCommonPrefix(String s1, String s2) {
+    //     int i1 = 0;
+    //     int i2 = 0;
+    //     String commonPrefix = "";
+        
+    //     while (i1 < s1.length() && i2 < s2.length()) {
+    //         if (s1.charAt(i1) == s2.charAt(i2)) {
+    //             commonPrefix += s1.charAt(i1);
+    //             i1++;
+    //             i2++;
+    //         } else {
+    //             break;
+    //         }
+    //     }
+        
+    //     return commonPrefix;
+    // }
 }
