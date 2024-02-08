7ef861dffaf31120863253004008fb5d7fc48926
Ken Utsunomiya <kenutsunomiya08@gmail.com>
2024-02-08 22:58:36 +0900

    Time: 1 ms (40.65%) | Memory: 41.7 MB (32.22%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,18 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int lengthOfLastWord = 0;
-        int idx = 0;
-        while (idx < s.length()) {
-            int length = 0;
-            while (idx < s.length() && s.charAt(idx) != ' ') {
-                length++;
-                idx++;
-            }
-            if (length > 0) {
-                lengthOfLastWord = length;
-            }
-            idx++;
-        }
-        return lengthOfLastWord;
+        String[] splitS = s.trim().split(" ");
+        return splitS[splitS.length - 1].length();
     }
 }
